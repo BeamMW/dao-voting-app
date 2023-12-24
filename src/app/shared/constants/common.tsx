31f840c0f6544972bb26ddde59878428d5634bc3
Maxnflaxl <63856008+Maxnflaxl@users.noreply.github.com>
2023-12-25 00:11:29 +0100

    commit

--- src/app/shared/constants/common.tsx
+++ src/app/shared/constants/common.tsx
@@ -1,5 +1,5 @@
 
-export const CID = '8ecadf81c0446416c393bd3a3843693943057b26022e490c998759f827e6bb4a';
+export const CID = '64c8bbbd7c411bd7f9f9a0fd4ca678c581350b5b5ce0b0c055033c7e8f69e555';
 export const GROTHS_IN_BEAM = 100000000;
 export const PROPOSALS = {
     CURRENT: 'current',
